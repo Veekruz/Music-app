c49cd6779a59335ff7e893f66d50b7af0677a6e2
199Benji <benjaminmiracle332@gmail.com>
2025-11-04 14:55:44 +0100

    collaborator commit

--- src/Info.jsx
+++ src/Info.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Info = () => {
   return (
+    <>
     <div>This page was recently added to test Git Clone.</div>
+    <div>This line was push by Miracle Benjamin</div>
+
+    </>
   )
 }
 
